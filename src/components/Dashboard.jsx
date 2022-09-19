312f968970f9362a71d85731b9bbcc4245d9c992
marparven1 <marparven1@alum.us.es>
2022-09-19 11:40:16 +0200

    Cambios en el estilo

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -28,9 +28,7 @@ export const Dashboard = (props) => {
     <>
         <div className="DashboardMain" >
           <div >
-            <h1>DASHBOARD</h1>
           <a className="nav-link  h5"  href=" "  onClick={ cerrarSesion } > <FontAwesomeIcon icon={faUserLock}  /> Cerrar Sesión</a>
-          
             <iframe id ="DashScapi"  title="SCAPI_V8 - Radiación"  src="https://app.powerbi.com/view?r=eyJrIjoiMTkwNzcwOWYtOTk3Zi00NzIwLTlkM2QtMjhjM2Q0YmM5ZGU3IiwidCI6IjcwYWIxMGRjLWI0MDMtNDU0Mi05NGRlLTJkM2Q4YzUzZjMzNiIsImMiOjl9&embedImagePlaceholder=true" frameborder="0" allowFullScreen={true} ></iframe>
           </div>
         </div>
